62975826dc8a1c7d652a92d28330792fa73c4a98
vietnamvpn <kangst24h@gmail.com>
2026-08-01 23:40:40 +0800

    Refactor update script to use git fetch and reset for a clean sync

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,29 +5,36 @@ source "$INSTALL_DIR/scripts/utils.sh"
 
 check_root
 
-print_info "Đang cập nhật mã nguồn quản lý từ GitHub..."
-cd "$INSTALL_DIR" || exit
-git reset --hard
-git pull origin main
+main() {
+    print_info "Đang cập nhật mã nguồn quản lý từ GitHub..."
+    cd "$INSTALL_DIR" || exit
+    
+    # Sửa lại lệnh git để ép đồng bộ 100%
+    git fetch --all
+    git reset --hard origin/main
 
-# Tự động cấp lại quyền thực thi
-chmod +x "$INSTALL_DIR/scripts/"*.sh
-chmod +x /usr/local/bin/vvc
+    # Tự động cấp lại quyền thực thi
+    chmod +x "$INSTALL_DIR/scripts/"*.sh
+    chmod +x /usr/local/bin/vvc
 
-print_info "Đang cập nhật lõi Realm lên bản mới nhất..."
-REALM_URL="https://github.com/zhboner/realm/releases/latest/download/realm-x86_64-unknown-linux-gnu.tar.gz"
+    print_info "Đang cập nhật lõi Realm lên bản mới nhất..."
+    REALM_URL="https://github.com/zhboner/realm/releases/latest/download/realm-x86_64-unknown-linux-gnu.tar.gz"
 
-systemctl stop realm
+    systemctl stop realm
 
-cd /tmp || exit
-wget -qO realm.tar.gz "$REALM_URL"
-tar -xvf realm.tar.gz
-mv realm /usr/local/bin/realm
-chmod +x /usr/local/bin/realm
-rm -f realm.tar.gz
+    cd /tmp || exit
+    wget -qO realm.tar.gz "$REALM_URL"
+    tar -xvf realm.tar.gz
+    mv realm /usr/local/bin/realm
+    chmod +x /usr/local/bin/realm
+    rm -f realm.tar.gz
 
-systemctl start realm
-print_info "Cập nhật mã nguồn và lõi Realm hoàn tất!"
+    systemctl start realm
+    print_info "Cập nhật mã nguồn và lõi Realm hoàn tất!"
 
-# Tự động nạp lại giao diện menu mới sau khi cập nhật xong
-exec bash "$INSTALL_DIR/scripts/menu.sh"
+    # Tự động nạp lại giao diện menu mới sau khi cập nhật xong
+    exec bash "$INSTALL_DIR/scripts/menu.sh"
+}
+
+# Gọi hàm main để toàn bộ tiến trình chạy trên RAM
+main
